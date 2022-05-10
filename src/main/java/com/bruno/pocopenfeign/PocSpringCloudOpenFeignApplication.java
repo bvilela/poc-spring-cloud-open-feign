2433fdc8eae5f4f217b4002071f32f84599d280b
Bruno Vilela <brunovilela2008@hotmail.com>
2022-05-09 23:13:34 -0300

    init project

---
+++ src/main/java/com/bruno/pocopenfeign/PocSpringCloudOpenFeignApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.pocopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringCloudOpenFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringCloudOpenFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/pocopenfeign/PocSpringCloudOpenFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.pocopenfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringCloudOpenFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
